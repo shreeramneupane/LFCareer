22f9ecd4b3513a83d16485b1751d0a0dde14fed2
shreeramneupane <shreeramneupane0@gmail.com>
2016-03-31 14:08:14 +0545

    LFC-4 Refactor PositionController test

--- test/controllers/positionsControllerTest.js
+++ test/controllers/positionsControllerTest.js
@@ -4,7 +4,8 @@ var expect = require('chai').expect,
 request = require('supertest'),
 express = require('express'),
 db = require('../../db'),
-HttpStatus = require('http-status-codes');
+HttpStatus = require('http-status-codes'),
+uuid = require('node-uuid');
 
 var PositionsController = require('../../controllers/positionsController');
 
@@ -35,11 +36,13 @@ describe('List position, Controller action index, GET /positions/', function () 
 
   describe('when positions are present', function () {
     var position1 = {
+      id: uuid.v1(),
       title: "Title 1",
       description: 'Description 1',
       specification: 'Specification 1'
     },
     position2 = {
+      id: uuid.v1(),
       title: "Title 2",
       description: 'Description 2',
       specification: 'Specification 2'
@@ -62,9 +65,11 @@ describe('List position, Controller action index, GET /positions/', function () 
         var resPosition1 = res.body[0];
         var resPosition2 = res.body[1];
         expect(res.body.length).to.equal(2);
+        expect(resPosition1.id).to.equal(position1.id);
         expect(resPosition1.title).to.equal(position1.title);
         expect(resPosition1.description).to.equal(position1.description);
         expect(resPosition1.specification).to.equal(position1.specification);
+        expect(resPosition2.id).to.equal(position2.id);
         expect(resPosition2.title).to.equal(position2.title);
         expect(resPosition2.description).to.equal(position2.description);
         expect(resPosition2.specification).to.equal(position2.specification);
@@ -79,21 +84,19 @@ describe('Show position detail, Controller action show, GET /positions/:id', fun
 
   describe('when position is present', function () {
     var position = {
+      id: uuid.v1(),
       title: "Title 1",
       description: 'Description 1',
       specification: 'Specification 1'
     }, id;
 
     before(function () {
-      return db('position').returning('id').insert(position)
-      .then(function (response) {
-        id = response[0];
-      });
+      return db('position').insert(position).then();
     });
 
     it('should respond with json containing position information', function (done) {
       request(app)
-      .get('/positions/' + id)
+      .get('/positions/' + position.id)
       .set('Accept', 'application/json')
       .expect(HttpStatus.OK)
       .end(function (err, res) {
@@ -101,7 +104,7 @@ describe('Show position detail, Controller action show, GET /positions/:id', fun
           done(err);
         }
         var responsePosition = res.body;
-        expect(responsePosition.id).to.equal(id);
+        expect(responsePosition.id).to.equal(position.id);
         expect(responsePosition.title).to.equal(position.title);
         expect(responsePosition.description).to.equal(position.description);
         expect(responsePosition.specification).to.equal(position.specification);
@@ -135,11 +138,9 @@ describe('Create new position, Controller action create, POST /positions/', func
 
   describe('when position params are valid', function () {
     var positionParam = {
-      "position": {
-        "title": "Title 1",
-        "description": 'Description 1',
-        "specification": 'Specification 1'
-      }
+      title: "Title 1",
+      description: 'Description 1',
+      specification: 'Specification 1'
     };
 
     before(function () {
@@ -156,11 +157,10 @@ describe('Create new position, Controller action create, POST /positions/', func
         if (err) {
           done(err);
         }
-        var responsePosition = res.body,
-        position = positionParam.position;
-        expect(responsePosition.title).to.equal(position.title);
-        expect(responsePosition.description).to.equal(position.description);
-        expect(responsePosition.specification).to.equal(position.specification);
+        var responsePosition = res.body;
+        expect(responsePosition.title).to.equal(positionParam.title);
+        expect(responsePosition.description).to.equal(positionParam.description);
+        expect(responsePosition.specification).to.equal(positionParam.specification);
         done();
       });
     });
@@ -168,10 +168,8 @@ describe('Create new position, Controller action create, POST /positions/', func
 
   describe('when position params title, description and specification field is empty', function () {
     var positionParam = {
-      "position": {
-        "title": '',
-        "specification": ''
-      }
+      "title": '',
+      "specification": ''
     };
 
     it('should resond with error message on title field', function (done) {
@@ -194,12 +192,10 @@ describe('Create new position, Controller action create, POST /positions/', func
 
   describe('when position params contain not required parameter', function () {
     var positionParam = {
-      "position": {
-        "title": "Title 1",
-        "description": 'Description 1',
-        "specification": 'Specification 1',
-        "more": "This is more parameter"
-      }
+      "title": "Title 1",
+      "description": 'Description 1',
+      "specification": 'Specification 1',
+      "more": "This is more parameter"
     };
 
     it('should resond with error message on title field', function (done) {
@@ -225,29 +221,25 @@ describe('Update position, Controller action update, PUT /positions/:id', functi
 
   describe('when update position params are valid', function () {
     var position = {
+      id: uuid.v1(),
       title: "Title 1",
       description: 'Description 1',
       specification: 'Specification 1'
-    }, id;
+    };
 
     before(function () {
-      return db('position').returning('id').insert(position)
-      .then(function (response) {
-        id = response[0];
-      });
+      return db('position').returning('id').insert(position).then();
     });
 
     var positionParam = {
-      position: {
-        title: "New Position title",
-        description: 'New Description',
-        specification: 'New Specification'
-      }
+      title: "New Position title",
+      description: 'New Description',
+      specification: 'New Specification'
     };
 
     it('should update the a position and respond with json containing position updated information', function (done) {
       request(app)
-      .put('/positions/' + id)
+      .put('/positions/' + position.id)
       .send(positionParam)
       .set('Accept', 'application/json')
       .expect(HttpStatus.OK)
@@ -255,9 +247,9 @@ describe('Update position, Controller action update, PUT /positions/:id', functi
         if (err) {
           done(err);
         }
-        expect(res.body.title).to.equal(positionParam.position.title);
-        expect(res.body.description).to.equal(positionParam.position.description);
-        expect(res.body.specification).to.equal(positionParam.position.specification);
+        expect(res.body.title).to.equal(positionParam.title);
+        expect(res.body.description).to.equal(positionParam.description);
+        expect(res.body.specification).to.equal(positionParam.specification);
         done();
       });
     });
