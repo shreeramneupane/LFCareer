25dcd8cdc43f74de56a946c756876ac640148675
shobhab shrestha <shobhabstha@gmail.com>
2016-03-31 10:07:22 +0545

    LFC-2 deleted lines

--- app.js
+++ app.js
@@ -15,23 +15,7 @@
   var routes = requireDir('./routes');
   for (var i in routes) app.use('/api', routes[i]);
 
-//  var upload = multer({
-//    storage: s3({
-//      dirname: 'uploads/resume',
-//      bucket: 's3.amazonaws.com/com.lftechnology.career',
-//      secretAccessKey: 'qfwQWVUwt768xiDPAlCvUmRsOeFpcXK21HYAccKo',
-//      accessKeyId: 'AKIAISAQ5F3H2CD42MRA',
-//      region: 'us-east-1',
-//      filename: function (req, file, cb) {
-//        cb(null, Date.now())
-//      }
-//    })
-//  });
-//
-//  app.post('/upload', upload.array('resume'), function (req, res, next) {
-//    console.log(res);
-//    res.send("Uploaded!");
-//  });
+
 
 
 
