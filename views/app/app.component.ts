474e0c3f12759b24c5ec293d3ab293ecea0615ca
shreeramneupane <shreeramneupane0@gmail.com>
2016-04-25 14:09:39 +0545

    LFC-FE Design for Dashboard

--- views/app/app.component.ts
+++ views/app/app.component.ts
@@ -2,6 +2,6 @@ import {Component} from 'angular2/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>Leapfrog Recruitment System</h1>'
+    templateUrl: 'app/app.component.html'
 })
 export class AppComponent { }
