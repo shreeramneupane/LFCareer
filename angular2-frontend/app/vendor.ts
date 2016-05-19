2fb1f78dab10ff64b26fa926c47a4d4db6df1440
bishalshr <bishalshrestha@lftechnology.com>
2016-05-19 15:21:14 +0545

    LFC-16 Reformat folder structure

--- angular2-frontend/app/vendor.ts
+++ angular2-frontend/app/vendor.ts
@@ -5,13 +5,14 @@ import '@angular/core';
 import '@angular/common';
 import '@angular/http';
 import '@angular/router-deprecated';
+
 // RxJS
 import 'rxjs';
 
-import './assets/stylesheets/bootstrap.min.css';
-import './assets/stylesheets/main.css';
-import './assets/stylesheets/custom.css';
+import 'shared/stylesheets/bootstrap.min.css';
+import 'shared/stylesheets/main.css';
+import 'shared/stylesheets/custom.css';
 
 import '../node_modules/bootstrap/dist/js/bootstrap.min.js';
-import './assets/vendor/javascripts/plugins.js';
+import 'shared/vendor/javascripts/plugins.js';
 import '../node_modules/toastr/build/toastr.min.css';
