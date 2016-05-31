cccd00c80ca6f5067d696f59a4f115615432805c
shreeramneupane <shreeramneupane0@gmail.com>
2016-05-31 14:07:20 +0545

    REFACTOR refactor job service, validation and model

--- services/jobService.js
+++ services/jobService.js
@@ -1,79 +1,77 @@
-;(function () {
-    "use strict";
+"use strict";
 
-    var checkit = require('checkit');
-    var uuid = require('node-uuid');
-    var validation = new checkit(require('../validation/jobValidation'));
-    var Job = require('../models/job');
-    var _ = require('lodash');
-    var Promise = require("bluebird");
-    var AppError = require('../error/AppError');
+var Checkit = require('checkit');
+var UUID = require('node-uuid');
+var Promise = require("bluebird");
 
-    module.exports = {
+var Job = require('../models/job');
+var AppError = require('../error/AppError');
+var Validation = new Checkit(require('../validation/jobValidation'));
 
-        list: function () {
-            return new Promise(function (resolve, reject) {
-                Job.list()
-                .then(function (response) {
-                    resolve(response);
-                })
-                .catch(function (err) {
-                    reject(err);
-                });
-            });
-        },
+module.exports = {
 
-        show: function (id) {
-            return new Promise(function (resolve, reject) {
-                Job.show(id)
-                .then(function (response) {
-                    resolve(response);
-                })
-                .catch(function (err) {
-                    reject(err);
-                });
-            });
-        },
+  list: function () {
+    return new Promise(function (resolve, reject) {
+      Job.list()
+      .then(function (response) {
+        resolve(response);
+      })
+      .catch(function (err) {
+        reject(err);
+      });
+    });
+  },
 
-        create: function (job) {
-            var job = _(job).omitBy(_.isUndefined).omitBy(_.isNull).omitBy(_.isEmpty).value();
-            return new Promise(function (resolve, reject) {
-                validation.run(job)
-                .then(function () {
-                    job.id = uuid.v1();
-                    Job.create(job)
-                    .then(function (response) {
-                        resolve(response);
-                    })
-                    .catch(function (err) {
-                        reject(err);
-                    });
-                })
-                .catch(function (err) {
-                    var error = AppError.validationError(err);
-                    reject(error);
-                });
-            });
-        },
+  show: function (id) {
+    return new Promise(function (resolve, reject) {
+      Job.show(id)
+      .then(function (response) {
+        resolve(response);
+      })
+      .catch(function (err) {
+        reject(err);
+      });
+    });
+  },
 
-        update: function (id, job) {
-            var job = _(job).omitBy(_.isUndefined).omitBy(_.isNull).omitBy(_.isEmpty).value();
-            return new Promise(function (resolve, reject) {
-                validation.run(job)
-                .then(function () {
-                    Job.update(id, job)
-                    .then(function (response) {
-                        resolve(response);
-                    })
-                    .catch(function (err) {
-                        reject(err);
-                    });
-                })
-                .catch(function (err) {
-                    var error = AppError.validationError(err);
-                    reject(error);
-                });
-            });
-        }
-    }
-})();
+  create: function (job) {
+    return new Promise(function (resolve, reject) {
+      Validation.run(job)
+      .then(function () {
+        job.id = UUID.v1();
+        job.created_at = new Date();
+
+        Job.create(job)
+        .then(function (response) {
+          resolve(response);
+        })
+        .catch(function (err) {
+          reject(err);
+        });
+      })
+      .catch(function (err) {
+        var error = AppError.validationError(err);
+        reject(error);
+      });
+    });
+  },
+
+  update: function (id, job) {
+    return new Promise(function (resolve, reject) {
+      Validation.run(job)
+      .then(function () {
+        Job.update(id, job)
+        .then(function (response) {
+          resolve(response);
+        })
+        .catch(function (err) {
+          reject(err);
+        });
+      })
+      .catch(function (err) {
+        var error = AppError.validationError(err);
+        reject(error);
+      });
+    });
+  }
+};
--- validation/jobValidation.js
+++ validation/jobValidation.js
@@ -1,35 +1,30 @@
-
 module.exports = {
-    title: {
-        rule: 'required',
-        message: 'Please provide job title.'
-    },
-    intro: {
-        rule: 'required',
-        message: 'Please provide job intro.'
-    },
-    number_of_opening: {
-        rule: 'required',
-        message: 'Please provide number of openings.'
-    },
-    description: {
-        rule: 'required',
-        message: 'Please provide job description.'
-    },
-    specification: {
-        rule: 'required',
-        message: 'Please provide job specification.'
-    },
-    created_date: {
-        rule: 'required',
-        message: 'Please provide job creation date.'
-    },
-    valid_until: {
-        rule: 'required',
-        message: 'Please provide job closing date.'
-    },
-    position_id: {
-        rule: 'required',
-        message: 'Please provide job closing date.'
-    }
+  title: {
+    rule: 'required',
+    message: 'Please provide job title.'
+  },
+  intro: {
+    rule: 'required',
+    message: 'Please provide job intro.'
+  },
+  number_of_opening: {
+    rule: 'required',
+    message: 'Please provide number of openings.'
+  },
+  description: {
+    rule: 'required',
+    message: 'Please provide job description.'
+  },
+  specification: {
+    rule: 'required',
+    message: 'Please provide job specification.'
+  },
+  valid_until: {
+    rule: 'required',
+    message: 'Please provide job closing date.'
+  },
+  position_id: {
+    rule: 'required',
+    message: 'Please provide job closing date.'
+  }
 };
