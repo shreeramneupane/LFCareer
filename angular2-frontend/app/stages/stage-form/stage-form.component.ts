22ac91bc80c03be7b853ec127f2ad92802911fa6
bishalshr <bishalshrestha@lftechnology.com>
2016-05-31 16:12:27 +0545

    LFC-FE-2 Add form validation for stage

--- angular2-frontend/app/stages/stage-form/stage-form.component.ts
+++ angular2-frontend/app/stages/stage-form/stage-form.component.ts
@@ -1,21 +1,31 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { NgForm }                                 from '@angular/common';
+import { Control, ControlGroup, Validators }                                 from '@angular/common';
 import { ROUTER_DIRECTIVES }                      from '@angular/router-deprecated';
-
+import { ControlMessages } from '../../shared/components/control-messages';
 import { Stage } from '../shared/stage';
 
 @Component({
   selector   : 'stage-form',
   templateUrl: 'app/stages/stage-form/stage-form.component.html',
-  providers  : [NgForm],
-  directives : [ROUTER_DIRECTIVES]
+  directives : [ROUTER_DIRECTIVES, ControlMessages]
 })
 
 export class StageFormComponent {
   @Input() stage:Stage;
   @Output() onSubmit = new EventEmitter<Stage>();
 
+  submitted:boolean = false;
+  ctrlName:Control = new Control('', Validators.required);
+
+  formGroup:ControlGroup = new ControlGroup({
+    name: this.ctrlName
+  });
+
   submit(stage:Stage) {
-    this.onSubmit.emit(stage);
+    this.submitted = true;
+    if (this.formGroup.valid)
+      this.onSubmit.emit(stage);
+    else
+      toastr.error('Please fill the required fields', 'Error!');
   }
 }
