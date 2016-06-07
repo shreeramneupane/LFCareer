df91f2ec5e974c6dad777d8da60a6925e682d3c9
bishalshr <bishalshrestha@lftechnology.com>
2016-06-07 09:18:48 +0545

    LFC-FE-2 Implement collapsible in applicant show page

--- angular2-frontend/app/applicants/applicant-show/applicant-show.component.ts
+++ angular2-frontend/app/applicants/applicant-show/applicant-show.component.ts
@@ -1,8 +1,11 @@
 import {Component} from '@angular/core';
 
+import { PageHeader } from '../../shared/components/page-header/pageHeader.component';
 @Component({
-  selector: 'applicant-show',
-  templateUrl: 'app/applicants/applicant-show/applicant-show.component.html'
+  selector   : 'applicant-show',
+  styleUrls  : ['app/applicants/applicant-show/applicant-show.component.css'],
+  templateUrl: 'app/applicants/applicant-show/applicant-show.component.html',
+  directives: [PageHeader]
 })
 export class ApplicantShowComponent {
 }
