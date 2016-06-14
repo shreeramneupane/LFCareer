63dc962ff03bf813f46a1755e5ea4cd0b16f6202
bishalshr <bishalshrestha@lftechnology.com>
2016-06-14 10:09:20 +0545

    LFC-FE-2 Add hiring pipeline

--- angular2-frontend/app/applicants/applicant-show/applicant-show.component.ts
+++ angular2-frontend/app/applicants/applicant-show/applicant-show.component.ts
@@ -4,6 +4,7 @@ import { Achievements } from './achievements/achievements.component.ts';
 import { Educations } from './educations/educations.component';
 import { Experiences } from './experiences/experiences.component';
 import { GeneralDetails } from './general-details/general-details.component';
+import { HiringPipeline } from './hiring-pipeline/hiring-pipeline.component';
 import { OtherDetails } from './others/others.component';
 import { PageHeader } from '../../shared/components/page-header/pageHeader.component';
 import { Profile } from './profile/profile.component';
@@ -14,7 +15,7 @@ import { References } from './references/references.component';
   selector  : 'applicant-show',
   styles    : [require('./applicant-show.component.css')],
   template  : require('./applicant-show.component.html'),
-  directives: [PageHeader, Achievements, Educations, Experiences, GeneralDetails, OtherDetails, Portfolios, Profile, References]
+  directives: [PageHeader, Achievements, Educations, Experiences, GeneralDetails, HiringPipeline, OtherDetails, Portfolios, Profile, References]
 })
 export class ApplicantShowComponent {
 }
---
+++ angular2-frontend/app/applicants/applicant-show/hiring-pipeline/hiring-pipeline.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'hiring-pipeline',
+  styles  : [require('../applicant-show.component.css')],
+  template: require('./hiring-pipeline.component.html'),
+})
+
+export class HiringPipeline {
+}
