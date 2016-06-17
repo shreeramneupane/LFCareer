7369b4b2985c57c9163fdd9e380f736cd07a0eb1
shreeramneupane <shreeramneupane0@gmail.com>
2016-06-17 10:29:11 +0545

    LFC-5 Response by including job's stage on job create and update

--- services/jobService.js
+++ services/jobService.js
@@ -7,7 +7,7 @@ var models = require('../models/index');
 
 var JobStageService = require('../services/jobStageService');
 
-module.exports = {
+var JobService = {
 
   list: function () {
     return new Promise(function (resolve, reject) {
@@ -36,8 +36,10 @@ module.exports = {
         delete job.Stages;
         _.each(job.stages, function (stage) {
           stage.precedence_number = stage.JobStage.precedence_number;
+          stage.is_active = stage.JobStage.is_active;
         });
-        job.stages = _.sortBy(_.map(job.stages, _.partialRight(_.pick, 'id', "title", "precedence_number")), 'precedence_number');
+        job.stages = _.map(job.stages, _.partialRight(_.pick, 'id', "title", "precedence_number", "is_active"));
+        job.stages = _.sortBy(job.stages, 'precedence_number');
         resolve({job: job});
       })
       .catch(function (err) {
@@ -50,12 +52,12 @@ module.exports = {
     var jobID;
 
     return new Promise(function (resolve, reject) {
-      return models.sequelize.transaction()
+      models.sequelize.transaction()
       .then(function (t) {
         return models.Job.create(jobParam, {transaction: t})
         .then(function (job) {
           jobID = job.id;
-          return validatePresenceOfAllDefaultStageWithPrecedence(jobParam['stages'])
+          return JobStageService.validatePresenceOfAllDefaultStageWithPrecedence(jobParam['stages'])
           .then(function () {
             return JobStageService.createMultiple(jobID, jobParam['stages'], t)
           });
@@ -69,14 +71,10 @@ module.exports = {
         });
       })
       .then(function () {
-        models.Job.findOne({
-          where: {
-            id: jobID
-          }
-        })
-        .then(function (job) {
-          resolve({job: job});
-        })
+        JobService.show(jobID)
+        .then(function (createdJob) {
+          resolve(createdJob);
+        });
       })
       .catch(function (err) {
         reject(err);
@@ -86,6 +84,8 @@ module.exports = {
   },
 
   update: function (id, jobParam) {
+    var jobID;
+
     return new Promise(function (resolve, reject) {
       models.Job.find({
         where: {
@@ -94,11 +94,34 @@ module.exports = {
       })
       .then(function (job) {
         if (job) {
-          job.updateAttributes(jobParam)
-          .then(function (response) {
-            resolve({job: response});
+          return models.sequelize.transaction()
+          .then(function (t) {
+            return job.updateAttributes(jobParam, {transaction: t})
+            .then(function (job) {
+              jobID = job.id;
+              return JobStageService.validatePresenceOfAllDefaultStageWithPrecedence(jobParam['stages'])
+              .then(function () {
+                return JobStageService.updateMultiple(jobID, jobParam['stages'], t)
+              });
+            })
+            .then(function () {
+              return t.commit();
+            })
+            .catch(function (err) {
+              t.rollback();
+              throw new Error(err);
+            });
           });
         }
+        else {
+          throw new Error("Can't find job with provided id.")
+        }
+      })
+      .then(function () {
+        JobService.show(jobID)
+        .then(function (job) {
+          resolve(job);
+        })
       })
       .catch(function (err) {
         reject(err);
@@ -107,45 +130,4 @@ module.exports = {
   }
 };
 
-function validatePresenceOfAllDefaultStageWithPrecedence(jobStageParams) {
-  return new Promise(function (resolve, reject) {
-    var stages = _.map(jobStageParams, _.partialRight(_.pick, 'id', "precedence_number"));
-    if (_.uniqBy(stages, 'id').length === stages.length && _.uniqBy(stages, 'precedence_number').length === stages.length) {
-      var sortedStages = _.sortBy(stages, 'precedence_number');
-      var previousPrecedenceNumber = 0;
-
-      models.Stage.findAll({
-        where: {is_default: true},
-        attributes: ['id', 'precedence_number']
-      }).then(function (response) {
-        var defaultStages = _.sortBy(_.map(response, 'dataValues'), 'precedence_number');
-        var defaultStagesIDs = _.map(defaultStages, "id");
-
-        var sortedStagesOfDefaultType = [];
-        sortedStages.forEach(function (sortedStage) {
-          if (defaultStagesIDs.indexOf(sortedStage.id) > -1) {
-            sortedStagesOfDefaultType.push(sortedStage);
-          }
-        });
-
-        _.forEach(sortedStagesOfDefaultType, function (sortedStage) {
-          previousPrecedenceNumber = previousPrecedenceNumber + 1;
-          sortedStage.precedence_number = previousPrecedenceNumber;
-        });
-
-        if (_.isEqual(defaultStages, sortedStagesOfDefaultType)) {
-          resolve(true);
-        }
-        else {
-          reject(new Error("Please ensure to include all default stage with out fluctuating default precedence"));
-        }
-      })
-      .catch(function (err) {
-        reject(err);
-      })
-    }
-    else {
-      reject(new Error("Please be sure not to duplicate stage or precedence."));
-    }
-  });
-}
+module.exports = JobService;
