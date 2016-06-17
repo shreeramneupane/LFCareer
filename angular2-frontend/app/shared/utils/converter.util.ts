61e16c9b9fd35f1473eadad0f9f7556ee46ca225
bishalshr <bishalshrestha@lftechnology.com>
2016-06-17 09:44:44 +0545

    LFC-FE-5 Remove unnecessary codes from converter util

--- angular2-frontend/app/shared/utils/converter.util.ts
+++ angular2-frontend/app/shared/utils/converter.util.ts
@@ -6,11 +6,9 @@ export class Converter {
     if (typeof(data) != 'object') {
       return '?' + data;
     }
-    console.log(data.hasOwnProperty('start'))
     var str = [];
     for (var p in data) {
       if (data[p] != null && data.hasOwnProperty(p)) {
-        console.log(p, data[p])
         str.push(encodeURIComponent(p) + '=' + encodeURIComponent(data[p]));
       }
     }
