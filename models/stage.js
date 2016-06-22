c3c0fd877161a140950d3f515f8109c0a9970dae
shreeramneupane <shreeramneupane0@gmail.com>
2016-06-22 17:16:20 +0545

    LFC-33 List the stage that are applicable for the applicant

--- models/stage.js
+++ models/stage.js
@@ -28,6 +28,7 @@ module.exports = function (sequelize, DataTypes) {
   }, {
     classMethods: {
       associate: function (models) {
+        Stage.hasMany(models.JobStage);
         Stage.belongsToMany(models.Job, {through: models.JobStage});
         Stage.belongsToMany(models.Applicant, {through: models.ApplicantStage});
       }
