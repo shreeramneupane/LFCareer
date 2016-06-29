2df375cc5c187be89f69a5c300b1143e7c5db97a
shreeramneupane <shreeramneupane0@gmail.com>
2016-06-29 11:20:05 +0545

    LFC-33 Add review to applicant stage

--- routes/applicantStageRoute.js
+++ routes/applicantStageRoute.js
@@ -4,9 +4,11 @@ var express = require('express'),
 router = express.Router();
 
 var ApplicantStageController = require('../controllers/applicantStageController');
+var ApplicantStageReviewController = require('../controllers/applicantStageReviewController');
 
 router.get('/applicants/:applicant_id/timeline', ApplicantStageController.timeline);
 router.get('/applicants/:applicant_id/stages', ApplicantStageController.index);
 router.post('/applicants/:applicant_id/stages', ApplicantStageController.create);
+router.post('/applicant_stages/:applicant_stage_id/review', ApplicantStageReviewController.create);
 
 module.exports = router;
--- services/applicantStageRemarkService.js
+++ services/applicantStageRemarkService.js
@@ -11,5 +11,43 @@ module.exports = {
         remark: remark
       }, {transaction: t})
     }
+  },
+
+  add: function (applicantStageID, remark) {
+    return new Promise(function (resolve, reject) {
+      models.ApplicantStageRemark.findOne({
+        where: {
+          applicant_stage_id: applicantStageID
+        }
+      })
+      .then(function (applicantStageReview) {
+        if (applicantStageReview) {
+          return models.ApplicantStageRemark.update({remark: remark}, {
+            where: {
+              applicant_stage_id: applicantStageID
+            }
+          })
+          .then(function () {
+            return models.ApplicantStageRemark.findOne({
+              where: {
+                applicant_stage_id: applicantStageID
+              }
+            })
+          });
+        }
+        else {
+          return models.ApplicantStageRemark.create({
+            applicant_stage_id: applicantStageID,
+            remark: remark
+          });
+        }
+      })
+      .then(function (response) {
+        resolve({remark: response})
+      })
+      .catch(function (err) {
+        reject(err);
+      })
+    });
   }
 };
